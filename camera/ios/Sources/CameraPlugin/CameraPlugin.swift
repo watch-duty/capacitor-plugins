1c552fb1f5c0fcc95d3b8c22b00213aa6282ecb5
David Watkins <dwat91@gmail.com>
2021-08-28 22:44:49 -0700

    Publish to @watchduty/camera Package (#1)
    * Use watchduty name in npm org
    
    * Fix swiftlint
    
    * Try CapacitorCamera > WatchdutyCamera
    
    * Bump version

--- camera/ios/Sources/CameraPlugin/CameraPlugin.swift
+++ camera/ios/Sources/CameraPlugin/CameraPlugin.swift
@@ -381,7 +381,7 @@ private extension CameraPlugin {
     }
 
     func showPrompt() {
-        Locator.shared.authorize();
+        Locator.shared.authorize()
         Locator.shared.getLocation { result in
             print("Location Result: \(String(describing: result))")
             switch result {
@@ -393,7 +393,7 @@ private extension CameraPlugin {
                     print(error)
             }
          }
-        
+
         Locator.shared.getHeading { result in
             print("Heading Result: \(String(describing: result))")
             switch result {
@@ -563,7 +563,7 @@ private extension CameraPlugin {
         metadata[kCGImagePropertyGPSDictionary as String] = self.currentLocation.exifMetadata(heading: self.currentHeading)
 
         print("Meta Data: \(String(describing: metadata))")
-        
+
         // get the result
         var result = processedImage(from: image, with: metadata)
         result.flags = flags
